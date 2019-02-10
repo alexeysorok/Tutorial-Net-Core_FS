64e5d96f3244dd70e5328c1bf22f6645bdc44e00
alexe <alexe@W10SSD>
2019-02-10 21:31:36 +0300

    Добавьте файлы проекта.

---
+++ Tutorial-Net-Core_FS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial_Net_Core_FS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
