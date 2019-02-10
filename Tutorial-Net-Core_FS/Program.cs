e603b445de19de242acd36eec86cd55a750ebab2
alexe <alexe@W10SSD>
2019-02-10 21:41:40 +0300

    Метод для работы с диреткориями

--- Tutorial-Net-Core_FS/Program.cs
+++ Tutorial-Net-Core_FS/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using static System.Console;
+using System.IO;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
 
 namespace Tutorial_Net_Core_FS
 {
@@ -6,7 +10,26 @@ namespace Tutorial_Net_Core_FS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OutputFileSystemInfo();
         }
+
+
+        // работа с путями 
+        static void OutputFileSystemInfo()
+        {
+            WriteLine($"Path.PathSeparator: {PathSeparator}");
+            WriteLine($"Path.DirectorySeparatorChar: {DirectorySeparatorChar}");
+            WriteLine($"Directory.GetCurrentDirectory(): {GetCurrentDirectory()}");
+            WriteLine($"Environment.CurrentDirectory: {CurrentDirectory}");
+            WriteLine($"Environment.SystemDirectory: {SystemDirectory}");
+            WriteLine($"Path.GetTempPath(): {GetTempPath()}");
+            WriteLine($"GetFolderPath(SpecialFolder):");
+            WriteLine($" System: {GetFolderPath(SpecialFolder.System)}");
+            WriteLine($" ApplicationData:{ GetFolderPath(SpecialFolder.ApplicationData)}");
+            WriteLine($" MyDocuments: {GetFolderPath(SpecialFolder.MyDocuments)}");
+            WriteLine($" Personal: {GetFolderPath(SpecialFolder.Personal)}");
+        }
+        // В типе Environment реализовано много других полезных членов, в том числе
+        // метод GetEnvironmentVariables, а также свойства OSVersion и ProcessorCount
     }
 }
