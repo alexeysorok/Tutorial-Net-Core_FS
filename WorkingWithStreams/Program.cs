9eac1c0d7133a359d0810f5ef08513b98f3277a0
alexe <alexe@W10SSD>
2019-02-10 22:05:42 +0300

    запись в тектовый файл

---
+++ WorkingWithStreams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        // определение массива позывных пилота Viper
+        static string[] callsigns = new string[] { "Husker", "Starbuck",
+            "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" };
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        // запись в текстовый файл 
+        static void WorkWithText()
+        {
+            // определение файла для записи
+            string textFile = Combine(CurrentDirectory, "streams.txt");
+            // создание текстового файла и возвращение помощника записи
+            StreamWriter text = File.CreateText(textFile);
+            // перечисление строк с записью каждой из них в поток в отдельной строке
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+            text.Close(); // release resources
+                          // вывод содержимого файла в консоль
+            WriteLine($"{textFile} contains { new FileInfo(textFile).Length} bytes.");
+            WriteLine(File.ReadAllText(textFile));
+        }
+
+    }
+}
